f2303f2153834ef7fd7bc583e54eb53c8f9509a7
b044113 <marcesantos@gmail.com>
2019-01-12 20:24:46 -0300

    First Version

--- buscacole/views.py
+++ buscacole/views.py
@@ -13,10 +13,16 @@ def search_form(request):
     return render(request, 'buscacole/formulario_busqueda.html', {'levels': levels})
 
 def search(request):
+    """
+
+    :param request:
+    :return: Realiza la búsqueda de los colegios a partir de la zona y el nivel/grado
+
+    """
     if 'q' in request.GET and request.GET['q'] \
-            and 'level' in request.GET and request.GET['level']:
+            and 'grade' in request.GET and request.GET['grade']:
         q = request.GET['q']
-        level_id = request.GET['level']
+        grade_id = request.GET['grade']
 
         query_list = q.split()
         schools = School.objects.filter(functools.reduce(operator.and_,
@@ -26,7 +32,7 @@ def search(request):
                                         functools.reduce(operator.and_,
                                             (Q(province__name__icontains=q) for q in query_list))
                                         )
-        schools = schools.filter(Q(vacancy__grade__level = level_id) & Q(vacancy__vacancies__gt = 0))
+        schools = schools.filter(Q(vacancy__grade__id = grade_id) & Q(vacancy__vacancies__gt = 0)).distinct
 
         return render(request, 'escuelas/school_list.html',
                       {'school_list': schools, 'query': q})
@@ -35,33 +41,17 @@ def search(request):
 
 
 def load_grades(request):
+    """
+
+    :param request:
+    :return: Rellena los valores de los grados a partir del nivel seleccionado
+    utilizando el archivo grade_dropdown_list_option.html
+
+    """
     if 'level' in request.GET and request.GET['level']:
         level_id = request.GET['level']
-        grades = Grade.objects.filter(level_id=level_id).order_by('name')
-        return render(request, 'hr/grade_dropdown_list_options.html', {'grades': grades})
+        grades = Grade.objects.filter(level__id=level_id).order_by('name')
+        return render(request, 'buscacole/grade_dropdown_list_options.html', {'grades': grades})
     else:
         return HttpResponse('Seleccione un nivel de educación.')
 
-
-
-class SchoolSearchListView(SchoolListView):
-    """
-    Display a School List page filtered by the search query.
-    """
-    paginate_by = 10
-
-    def get_queryset(self):
-        result = super(SchoolSearchListView, self).get_queryset()
-
-        query = self.request.GET.get('q')
-        if query:
-            query_list = query.split()
-            result = result.filter(
-                functools.reduce(operator.and_,
-                       (Q(title__icontains=q) for q in query_list)) |
-                functools.reduce(operator.and_,
-                       (Q(content__icontains=q) for q in query_list))
-            )
-
-        return result
-# Create your views here.
